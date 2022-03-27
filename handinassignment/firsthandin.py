56f73a83f31368f8fd8a5f129d880136cb34eabf
AlexKenfelt <cph-ak428@cphbusiness.dk>
2022-03-27 22:31:34 +0200

    task 4 almost done

--- handinassignment/firsthandin.py
+++ handinassignment/firsthandin.py
@@ -8,7 +8,7 @@ https://www.kaggle.com/sanjeetsinghnaik/top-1000-highest-grossing-movies
 
 3. Get the percentage of PG rated movies between 2001 and 2015
 
-4. Calculate the average of world sales for each genre and visualize the data with a bar chart. (Hint: use groupBy)
+4. Calculate the average of world sales for each Genre and visualize the data with a bar chart. (Hint: use groupBy)
 "
 """
 
@@ -34,13 +34,38 @@ print(df.sort_values('World Sales (in $)', ascending=False).head(10)[['World Sal
     # Todo: type out number 1-10 
 
 
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # (2.)
 # Create a pie chart that shows the distribution of Licenses (PG, R, M and so on)
 # I will have to use the data from the csv file, but only collected the relevant data (Licenses).
-license = pd.Series(data['License'])   
+#license = pd.Series(data['License'])   
 
 # I will use the License data in a pandaseries, so I can use it to count the value of the data, save it, sum it up and then be used in the pie chart.
-license_count = license.value_counts() 
-license_count.plot(kind='pie', autopct='%1.0f%%')
+#license_count = license.value_counts() 
+#license_count.plot(kind='pie', autopct='%1.0f%%')
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# (3.)
+df1 = data[['License','Release Date']]
+# I add a column 'Year'
+# normalize = true --> data_norm = (data - data.min())/ (data.max() - data.min())
+df1[['Release Date', 'Year']] = df1['Release Date'].str.split(',', 1, expand=True) 
+df1_filter = df1[(df1['Year'] >= " 2001") & (df1['Year'] < " 2015")]
+df1 = df1_filter['License'].value_counts(normalize=True).mul(100).round(1).astype(str) + '%'            
+print('PG = ' + df1.iloc[1]) 
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# (4.)
+def cal_avg():
+    df1 = df_movies [['License','Release Date', 'Genre', 'World Sales (in $)']]
+    df2 = pd.DataFrame(data, colums = ['World Sales (in $)', 'Genre'])
+    df3 = df2['Genre'].str.get_dummies(sep="")
+
+    print(df2)
+
+
+
